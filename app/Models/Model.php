d35d6a6a415adc1f583a6c4da1d602bb1f8feb62
Ali-Hassan-ALi <ah965961@gmail.com>
2021-07-22 14:17:22 +0300

    first task Setting up the project

---
+++ app/Models/Model.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Support\Str;
+
+class Model extends BaseModel
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    protected static function boot ()
+    {
+        parent::boot();
+
+        static::creating(function($model) {
+            // $model->id = str::uuid();
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    public $incrementing = false;
+
+    protected static function boot ()
+    {
+        parent::boot();
+
+        static::creating( function($model) {
+            // $model->id = str::uuid();
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
